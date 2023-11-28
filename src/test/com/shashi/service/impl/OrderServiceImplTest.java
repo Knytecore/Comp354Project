2fcd144fc75b105b043d75e1b8592c130bf105c9
Tabesh Haidary <haidary.tab@gmail.com>
2023-11-28 13:16:55 -0500

    Added unit test for getLeastSellingItems() and getLeastSellingItemsByType()

--- src/test/com/shashi/service/impl/OrderServiceImplTest.java
+++ src/test/com/shashi/service/impl/OrderServiceImplTest.java
@@ -101,11 +101,75 @@ class OrderServiceImplTest
     void getLeastSellingItems()
     {
         OrderServiceImpl osi = new OrderServiceImpl();
+        List<ProductBean> l = osi.getMostSellingItems();
+        Connection con = DBUtil.provideConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Iterator i = l.iterator();
+        int size = 0;
+
+        try
+        {
+            ps = con.prepareStatement("SELECT  p.* FROM product p LEFT JOIN orders o ON p.pid = o.prodid GROUP BY p.pid, p.pname, p.ptype, p.pinfo, p.pprice, p.pquantity, p.image, p.usedpquantity, p.usedpprice, p.discountpprice ORDER BY SUM(o.quantity) LIMIT 3");
+            rs = ps.executeQuery();
+
+            while (rs.next())
+            {
+                size++;
+                if (i.hasNext())
+                {
+                    assertEquals(rs.getString("pid"), (((ProductBean) i.next()).getProdId()));
+                }
+            }
+        }
+
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+        assertEquals(size, l.size());
+        DBUtil.closeConnection(con);
+        DBUtil.closeConnection(ps);
     }
 
     @Test
     void getLeastSellingItemsByType()
     {
+        String[] types = {"mobile", "laptop", "tv", "camera", "speaker", "tablet", "fan", "cooler"};
         OrderServiceImpl osi = new OrderServiceImpl();
+        Connection con = DBUtil.provideConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try
+        {
+            for (String s : types)
+            {
+                List<ProductBean> l = osi.getMostSellingItems(s);
+                Iterator i = l.iterator();
+                int size = 0;
+                ps = con.prepareStatement("SELECT  p.* FROM product p LEFT JOIN orders o ON p.pid = o.prodid GROUP BY p.pid, p.pname, p.ptype, p.pinfo, p.pprice, p.pquantity, p.image, p.usedpquantity, p.usedpprice, p.discountpprice ORDER BY SUM(o.quantity) LIMIT 3");
+                ps.setString(1, s);
+                rs = ps.executeQuery();
+                while (rs.next())
+                {
+                    size++;
+                    if (i.hasNext())
+                    {
+                        assertEquals(rs.getString("pid"), (((ProductBean) i.next()).getProdId()));
+                    }
+                }
+                assertEquals(size, l.size());
+            }
+        }
+
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+        DBUtil.closeConnection(con);
+        DBUtil.closeConnection(ps);
     }
 }
